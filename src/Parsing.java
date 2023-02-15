4c689b7815d56ced0b1ac8df02382b03755a0449
Suphanath Wangthip <suphanath_w@cmu.ac.th>
2023-02-15 15:09:48 +0700

    Parsing

--- src/Parsing.java
+++ src/Parsing.java
@@ -1,83 +1,207 @@
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class Parsing {
-    import java.util.NoSuchElementException;
+    private List<String> tokens;
+    private int currentToken;
 
-    public class Parsing implements Parser {
-        private final Tokenizer tkz;
-        public Parsing(Tokenizer tkz){
-            this.tkz = tkz;
+    public void parse(List<String> tokens) throws Exception {
+        this.tokens = tokens;
+        this.currentToken = 0;
+        plan();
+    }
+
+    private void plan() throws Exception {
+        while (currentToken < tokens.size()) {
+            statement();
         }
+    }
 
-        @Override
-        public Exp parse() throws SyntaxError {
-            try{
-                Exp rs = E();
-                if(tkz.hasNextToken())
-                    throw new SyntaxError("token " + tkz.peek() + " is not null");
-                return rs;
-            }catch (SyntaxError e){
-                throw e;
-            }
-        }
-
-        private Exp E() throws SyntaxError{
-            try{
-                Exp rs = T();
-                while (tkz.hasNextToken() && (tkz.peek("+") || tkz.peek("-"))){
-                    if(tkz.peek("+")){
-                        tkz.consume();
-                        rs = new Binary(rs, "+", T());
-                    } else {
-                        tkz.consume();
-                        rs = new Binary(rs, "-", T());
-                    }
+    private void statement() throws Exception {
+        String token = getNextToken();
+        switch (token) {
+            case "done":
+            case "relocate":
+                break;
+            case "move":
+                moveCommand();
+                break;
+            case "invest":
+            case "collect":
+                regionCommand();
+                break;
+            case "shoot":
+                attackCommand();
+                break;
+            case "{":
+                blockStatement();
+                break;
+            case "if":
+                ifStatement();
+                break;
+            case "while":
+                whileStatement();
+                break;
+            default:
+                if (isIdentifier(token)) {
+                    assignmentStatement();
+                } else {
+                    throw new Exception("Unexpected token: " + token);
                 }
-                return rs;
-            } catch (IllegalArgumentException | NoSuchElementException e){
-                throw new SyntaxError(e.getMessage());
-            }
         }
+    }
 
-        private Exp T() throws SyntaxError{
-            try{
-                Exp rs = F();
-                while (tkz.hasNextToken() &&(tkz.peek("*") || tkz.peek("/") || tkz.peek("%"))){
-                    if(tkz.peek("*")){
-                        tkz.consume();
-                        rs = new Binary(rs, "*", F());
-                    } else if (tkz.peek("/")) {
-                        tkz.consume();
-                        rs = new Binary(rs, "/", F());
-                    } else {
-                        tkz.consume();
-                        rs = new Binary(rs, "%", F());
-                    }
-                }
-                return rs;
-            }catch (IllegalArgumentException | NoSuchElementException e){
-                throw new SyntaxError(e.getMessage());
-            }
+    private void assignmentStatement() throws Exception {
+        match("=");
+        expression();
+    }
+
+    private void actionCommand() throws Exception {
+        // done and relocate do not have arguments
+        // move, invest, and collect have one argument
+        // shoot has two arguments
+        switch (getNextToken()) {
+            case "move":
+                direction();
+                break;
+            case "invest":
+            case "collect":
+                expression();
+                break;
+            case "shoot":
+                direction();
+                expression();
+                break;
+            default:
+                throw new Exception("Invalid action command");
         }
+    }
 
-        private Exp F() throws SyntaxError{
-            try{
-                if(Character.isDigit(tkz.peek().charAt(0))){
-                    return  new IntLit(Integer.parseInt(tkz.consume()));
-                }else if(Character.isAlphabetic(tkz.peek().charAt(0))){
-                    return new Var(tkz.consume());
-                }else {
-                    Exp rs = null;
-                    if(tkz.peek("(")) {
-                        tkz.consume("(");
-                        rs = E();
-                        tkz.consume(")");
-                    }
-                    return rs;
-                }
-            }catch (NoSuchElementException | NumberFormatException | SyntaxError s){
-                throw new SyntaxError(s.getMessage());
-            }
+    private void moveCommand() throws Exception {
+        direction();
+    }
+
+    private void regionCommand() throws Exception {
+        expression();
+    }
+
+    private void attackCommand() throws Exception {
+        direction();
+        expression();
+    }
+
+    private void direction() throws Exception {
+        switch (getNextToken()) {
+            case "up":
+            case "down":
+            case "upleft":
+            case "upright":
+            case "downleft":
+            case "downright":
+                break;
+            default:
+                throw new Exception("Invalid direction");
+        }
+    }
+
+    private void blockStatement() throws Exception {
+        while (!peekToken().equals("}")) {
+            statement();
+        }
+        match("}");
+    }
+
+    private void ifStatement() throws Exception {
+        match("(");
+        expression();
+        match(")");
+        statement();
+        if (peekToken().equals("else")) {
+            match("else");
+            statement();
+        }
+    }
+
+    private void whileStatement() throws Exception {
+        match("(");
+        expression();
+        match(")");
+        statement();
+    }
+
+    private void expression() throws Exception {
+        term();
+        while (isAddOp(peekToken())) {
+            getNextToken();
+            term();
+        }
+    }
+
+    private void term() throws Exception {
+        factor();
+        while (isMulOp(peekToken())) {
+            getNextToken();
+            factor();
+        }
+    }
+
+    private void factor() throws Exception {
+        power();
+        while (peekToken().equals("^")) {
+            getNextToken();
+            factor();
+        }
+    }
+
+    private void power() throws Exception {
+        String token = getNextToken();
+        if (isNumber(token) || isIdentifier(token)) {
+            // Number or identifier
+        } else if (token.equals("(")) {
+            expression();
+            match(")");
+        } else if (token.equals("opponent") || token.equals("nearby")) {
+            direction();
+        } else {
+            throw new Exception("Invalid expression");
+        }
+    }
+
+    private boolean isNumber(String token) {
+        return token.matches("\\d+");
+    }
+
+    private boolean isIdentifier(String token) {
+        return token.matches("[a-zA-Z][a-zA-Z0-9]*");
+    }
+
+    private boolean isAddOp(String token) {
+        return token.equals("+") || token.equals("-");
+    }
+
+    private boolean isMulOp(String token) {
+        return token.equals("*") || token.equals("/") || token.equals("%");
+    }
+
+    private String getNextToken() throws Exception {
+        if (currentToken >= tokens.size()) {
+            throw new Exception("Unexpected end of input");
+        }
+        return tokens.get(currentToken++);
+    }
+
+    private String peekToken() throws Exception {
+        if (currentToken >= tokens.size()) {
+            throw new Exception("Unexpected end of input");
+        }
+        return tokens.get(currentToken);
+    }
+
+    private void match(String expected) throws Exception {
+        if (peekToken().equals(expected)) {
+            getNextToken();
+        } else {
+            throw new Exception("Expected " + expected);
         }
     }
 }
+
