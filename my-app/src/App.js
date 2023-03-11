3eeaad9a7d10a29e09d59152fdbafdd39fa9ffd1
Suphanath <suphanath_w@cmu.ac.th>
2023-03-11 17:50:44 +0700

    fix error

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-import Canvas from "./components/Canvas";
+import Hexagon from "./components/Hexagon";
 
 function App() {
+  const board = [];
+  // generate rows
+  for (let i = 0; i < 8; i++) {
+    const row = [];
+    // generate columns
+    for (let j = 0; j < 8; j++) {
+      row.push(<div><Hexagon /></div>);
+    }
+    board.push(<div><Hexagon /></div>);
+  }
+
   return (
     <div className="App">
-      <Canvas />
+      <h1 style={{marginBottom: 50}}>UPBEAT</h1>
+      {board}
     </div>
   );
 }
---
+++ my-app/src/components/Hexagon.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './hexagon.css';
+
+function Hexagon() {
+  return (
+    <div>
+      <div className="honeycomb"> 
+        <div>
+          <div className="ibws-fix">
+            <div className="hexagon">
+            </div>
+            <div className="hexagon">
+            </div>
+            <div className="hexagon">
+            </div>
+            <div className="hexagon">
+            </div>
+            <div className="hexagon">
+            </div>
+            <div className="hexagon">
+            </div>
+            <div className="hexagon">
+            </div>
+            <div className="hexagon">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+  );
+}
+
+export default Hexagon;
+
