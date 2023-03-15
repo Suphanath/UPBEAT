34152546876a39705e90fe5fb13f432cb57b3aa9
Suphanath <suphanath_w@cmu.ac.th>
2023-03-15 15:51:15 +0700

    add main.js and change background

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,31 +2,26 @@ import React from 'react';
 import './App.css';
 import './components/hexagon.css';
 import Play from './Play';
+import Main from './Main';
 
 function App() {
   const headingStyle = {
-    textAlign: 'center',
+    justifyContent: 'center',
+    display: 'flex',
     fontFamily: 'Geostar Fill',
-    fontSize: '4rem',
     color: '#FFE600',
     fontSize: '50px',
     margin: 100,
   };
   return (
     <div>
-      {/* <h1 style={headingStyle}>UPBEAT</h1>
-        <ul style={{ display: 'flex', justifyContent: 'center', margin: 150 , fontSize: 35}}>
-          <li style={{ margin: '0 10px' }}>
-            <a href="/play" style={{textDecoration: 'none', color: 'white',fontFamily: 'Geostar Fill'}}>Play</a>
-          </li>
-          
+       <h1 style={headingStyle}>UPBEAT</h1>
+        <ul style={{ display: 'flex',textAlign: 'center', justifyContent: 'center', margin: 200 , fontSize: 35}}>
+            <a href="/play" style={{textAlign: 'center',textDecoration: 'none', color: 'white',fontFamily: 'Geostar Fill'}}>Play</a>
         </ul>
-        <ul style={{ display: 'flex', justifyContent: 'center', margin: 100 , fontSize: 35}}>
-          <li style={{ margin: '0 10px' }}>
-            <a href="/how-to-play" style={{textDecoration: 'none', color: 'white',fontFamily: 'Geostar Fill'}}>How to play</a>
-          </li>
-        </ul> */}
-        <Play/>
+        <ul style={{textAlign: 'center', display: 'flex', justifyContent: 'center', margin: 100 , fontSize: 35}}>
+            <a href="/how-to-play" style={{textAlign: 'center',textDecoration: 'none', color: 'white',fontFamily: 'Geostar Fill'}}>How to play</a>
+        </ul> 
     </div>
   );
 }
--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -1,19 +1,50 @@
-import React from "react";
-import Play from './Play';
+import React from 'react';
+import './App.css';
 
-function Main(){
-return (
-    <nav>
-        <ul>
-            <li>
-                <a to="/">Home</a>
-            </li>
-            <li>
-                <a herf={Play.js}>Play</a>
-            </li>
+function Main() {
+  const headingStyle = {
+    textAlign: 'center',
+    fontFamily: 'Geostar Fill',
+    fontSize: '4rem',
+    color: '#FFE600',
+    fontSize: '50px',
+    margin: 100,
+  };
+
+  return (
+    <div>
+        <h1 style={headingStyle}>UPBEAT</h1>
+        <ul style={{ display: 'flex', justifyContent: 'center', margin: 15 , fontSize: 35}}>
+          <li style={{ margin: '0 10px' }}>
+            <a href="/play" style={{textDecoration: 'none', color: '#BBFF85',fontFamily: 'Geostar Fill'}}>Player1</a>
+          </li>
         </ul>
-    </nav>
-    )
-};
+        <ul style={{ display: 'flex', justifyContent: 'center', margin: 15 , fontSize: 35}}>
+          <li style={{ margin: '0 10px' }}>
+            <a href="/play" style={{textDecoration: 'none', color: '#BBFF85',fontFamily: 'Geostar Fill'}}>Player2</a>
+          </li>
+        </ul>
+        <ul style={{ display: 'flex', justifyContent: 'center', margin: 15 , fontSize: 35}}>
+          <li style={{ margin: '0 10px' }}>
+            <a href="/play" style={{textDecoration: 'none', color: '#BBFF85',fontFamily: 'Geostar Fill'}}>Player3</a>
+          </li>
+        </ul>
+        <ul style={{ display: 'flex', justifyContent: 'center', margin: 15 , fontSize: 35}}>
+          <li style={{ margin: '0 10px' }}>
+            <a href="/play" style={{textDecoration: 'none', color: '#BBFF85',fontFamily: 'Geostar Fill'}}>Player4</a>
+          </li>
+        </ul>
+        <ul style={{ display: 'flex', justifyContent: 'center', margin: 100 , fontSize: 35}}>
+          <li style={{ margin: '0 10px' }}>
+            <a href="/how-to-play" style={{textDecoration: 'none', color: 'red',fontFamily: 'Geostar Fill'}}>Back to menu</a>
+          </li>
+        </ul> 
+    </div>
+  );
+}
+const link = document.createElement('link');
+  link.href = 'https://fonts.googleapis.com/css2?family=Geostar+Fill&display=swap';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
 
 export default Main;
